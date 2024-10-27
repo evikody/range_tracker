5d8b5255c34a4387ce821264c7c0237d89bedc26
unknown <evikody06@gmail.com>
2024-10-27 19:56:57 +0800

    Display the results when the input is invalid

--- range_tracker/range_tracker.py
+++ range_tracker/range_tracker.py
@@ -11,9 +11,22 @@ while True:
 
         if 1 <= int(number) <= 10:
             num_1_10 += 1
+        elif 11 <= int(number) <= 20:
+            num_11_20 += 1
+        elif 21 <= int(number) <= 30:
+            num_21_30 += 1
+        elif 31 <= int(number) <= 40:
+            num_31_40 += 1
+        elif 41 <= int(number) <= 50:
+            num_41_50 += 1
+
 
     else:
         print("Invalid input. Please enter a valid number from 1 to 50.")
         break
 
 print(f"1 - 10 = {num_1_10}")
+print(f"11 - 20 = {num_11_20}")
+print(f"21 - 30 = {num_21_30}")
+print(f"31 - 40 = {num_31_40}")
+print(f"41 - 50 = {num_41_50}")
