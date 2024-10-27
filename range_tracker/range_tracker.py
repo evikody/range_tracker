955de617a4b28d2d262bc7c88723305834ccdd59
unknown <evikody06@gmail.com>
2024-10-27 18:17:19 +0800

    A loop that will continue asking if yes or no is entered

--- range_tracker/range_tracker.py
+++ range_tracker/range_tracker.py
@@ -1,6 +1,17 @@
 while True:
     number = input("Please enter a number from 1 to 50: ")
     if number.isdigit() and 1 <= int(number) <= 50:
-        break
+        print("You entered a valid number.")
+
+        while True:
+            retry = input("Do you want to enter another number? ").lower()
+            if retry == "yes":
+                break
+            elif retry == "no":
+                print("Thank You!")
+                exit()
+            else:
+                print("Invalid. Please type yes or no.")
+
     else:
         print("Invalid input. Please enter a valid number from 1 to 50.")
