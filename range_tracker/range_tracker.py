fb50e8e31c3dddaee99b0ae275bb478ec3bcd6df
unknown <evikody06@gmail.com>
2024-10-27 19:53:06 +0800

    Remove the loop for retrying and initialize counters for each range

--- range_tracker/range_tracker.py
+++ range_tracker/range_tracker.py
@@ -1,17 +1,19 @@
+num_1_10 = 0
+num_11_20 = 0
+num_21_30 = 0
+num_31_40 = 0
+num_41_50 = 0
+
 while True:
     number = input("Please enter a number from 1 to 50: ")
     if number.isdigit() and 1 <= int(number) <= 50:
         print("You entered a valid number.")
 
-        while True:
-            retry = input("Do you want to enter another number? ").lower()
-            if retry == "yes":
-                break
-            elif retry == "no":
-                print("Thank You!")
-                exit()
-            else:
-                print("Invalid. Please type yes or no.")
+        if 1 <= int(number) <= 10:
+            num_1_10 += 1
 
     else:
         print("Invalid input. Please enter a valid number from 1 to 50.")
+        break
+
+print(f"1 - 10 = {num_1_10}")
